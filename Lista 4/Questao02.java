208a5ca890778333a4952c569427866458bdfc1b
Tayná Mariana <106122544+TaynaMariana@users.noreply.github.com>
2024-03-26 11:29:14 -0300

    Update Questao02.java

---
+++ Lista 4/Questao02.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Questao02 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        while (true) {
+            
+            System.out.print("Digite um número (ou zero/negativo para encerrar): ");
+            double num = entrada.nextDouble();
+            
+            
+            if (num <= 0) {
+                System.out.println("Encerrando o programa.");
+                break;
+            }
+            
+            
+            double quadrado = Math.pow(num, 2);
+            double cubo = Math.pow(num, 3);
+            double raizQuadrada = Math.sqrt(num);
+            
+            
+            System.out.println("Número: " + num);
+            System.out.println("Quadrado: " + quadrado);
+            System.out.println("Cubo: " + cubo);
+            System.out.println("Raiz Quadrada: " + raizQuadrada);
+            System.out.println();
+        }
+        
+        entrada.close();
+    }
+}
