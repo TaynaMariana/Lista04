1e69080e4653bceaa513162d811dbb3d46d170b9
Tayná Mariana <106122544+TaynaMariana@users.noreply.github.com>
2024-04-01 11:04:53 -0300

    Update Questao09.java

---
+++ Lista 4/Questao09.java
@@ -0,0 +1,21 @@
+/*Dados dois países, A com população igual a cinco milhões de pessoas, e taxa de natalidade de três por cento ao ano, e, B, com população igual a sete milhões 
+de pessoas e taxa de natalidade de dois por cento ao ano, escreva um programa em Java que calcule iterativamente e exiba em quantos anos a população de A ultrapassará 
+a população de B.*/
+
+public class Questao09 {
+    public static void main(String[] args) {
+        int populacaoA = 5000000; 
+        int populacaoB = 7000000;
+        double taxaNatalidadeA = 0.03;
+        double taxaNatalidadeB = 0.02; 
+        int anos = 0;
+
+        while (populacaoA <= populacaoB) {
+            populacaoA += populacaoA * taxaNatalidadeA;
+            populacaoB += populacaoB * taxaNatalidadeB;
+            anos++;
+        }
+
+        System.out.println("Levará " + anos + " anos para a população de A ultrapassar a população de B.");
+    }
+}
